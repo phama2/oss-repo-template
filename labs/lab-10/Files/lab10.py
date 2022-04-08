37d8e6e7ad3b4116e20f8e302974bf531cb7254a
Andy Pham <70230763+phama2@users.noreply.github.com>
2022-04-08 10:48:22 -0400

    added files

---
+++ labs/lab-10/Files/lab10.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  8 10:07:13 2022
+
+@author: AndyP
+"""
+
+from __future__ import absolute_import, division, print_function
+
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+from PIL import Image
+from PIL import ImageOps
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(tf.__version__)
+plt.plot((-2,4),(-6,6))
+plt.show()
